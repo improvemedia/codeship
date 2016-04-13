f90795754a4f9bc1fed9c613ad7e5cd69ec4e126
Sergey Vinogradov <pettsonchello@gmail.com>
2016-04-13 14:12:55 +0300

    Add mobile links

--- tank.js
+++ tank.js
@@ -1,30 +1,35 @@
-
 var system = require('system');
-var links = ['/',
-             '/products',
-             '/photos',
-             '/posts/11247-6-nestandartnyh-sistem-hraneniya-dlya-ochen-malenkoy-kvartiry',
-             '/posts/11238-36-idealnyh-svetilnikov-dlya-malenkoy-kuhni',
-             '/posts/11096-novyy-sposob-oformleniya-derevyannogo-doma-kottedzh-v-nikolo-prozorovo'
-            ];
+var links = [
+  '/',
+  '/products',
+  '/products?mobile=1',
+  '/photos',
+  '/photos?mobile=1',
+  '/products/27400',
+  '/products/27400?mobile=1',
+  '/posts/11247-6-nestandartnyh-sistem-hraneniya-dlya-ochen-malenkoy-kvartiry',
+  '/posts/11238-36-idealnyh-svetilnikov-dlya-malenkoy-kuhni',
+  '/posts/11096-novyy-sposob-oformleniya-derevyannogo-doma-kottedzh-v-nikolo-prozorovo',
+];
 
 var host = system.env.host || 'http://staging.inmyroom.ru';
 
-casper.options.pageSettings.loadImages  = false;
-casper.options.pageSettings.loadPlugins = false;
+casper.options.pageSettings.loadImages        = false;
+casper.options.pageSettings.javascriptEnabled = false;
+casper.options.pageSettings.loadPlugins       = false;
 casper.options.waitTimeout = 10000;
 
-casper.test.begin('links unauthorized', links.length, function suite(test) {
-    casper.start();
-    casper.setHttpAuth('imr', 'imr');
+casper.test.begin('links unauthorized', links.length, function(test) {
+  casper.start();
+  casper.setHttpAuth('imr', 'imr');
 
-    links.forEach(function(link, _i, _a) {
-      casper.thenOpen(host + link,function() {
-          casper.test.assertHttpStatus(200, link);
-      });
+  links.forEach(function(link) {
+    casper.thenOpen(host + link, function() {
+      casper.test.assertHttpStatus(200, link);
     });
+  });
 
-    casper.run(function() {
-        test.done();
-    });
+  casper.run(function() {
+    test.done();
+  });
 });
